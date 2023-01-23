a8efce7e9a4c7d60b2bdaa0eef1339d354934230
Anirudh Vemula <avemula@aurora.tech>
2023-01-22 22:31:41 -0500

    updates

--- src/plot_widetree_exp.py
+++ src/plot_widetree_exp.py
@@ -7,39 +7,43 @@ matplotlib.rcParams['pdf.use14corefonts'] = True
 matplotlib.rcParams['text.usetex'] = True
 plt.rcParams.update({'font.size': 15})
 
+CB_color_cycle = ['#377eb8', '#ff7f00', '#4daf4a',
+                  '#f781bf', '#a65628', '#984ea3',
+                  '#999999', '#e41a1c', '#dede00']
+
 def plot_data(seeds):
-    mle_losses = []
-    moment_based_losses = []
+    mle_weights = []
+    moment_based_weights = []
 
     for seed in seeds:
-        mle_losses.append(np.load(f"data/mle_{seed}.npy"))
-        moment_based_losses.append(np.load(f"data/moment_based_{seed}.npy"))
+        mle_weights.append(np.load(f"data/mle_{seed}.npy"))
+        moment_based_weights.append(np.load(f"data/moment_based_{seed}.npy"))
 
-    mle_mean_losses, mle_std_losses = np.mean(mle_losses, axis=0), np.std(mle_losses, axis=0)
-    moment_based_mean_losses, moment_based_std_losses = np.mean(moment_based_losses, axis=0), np.std(moment_based_losses, axis=0)
+    mle_mean_weights, mle_std_weights = np.mean(mle_weights, axis=0), np.std(mle_weights, axis=0)
+    moment_based_mean_weights, moment_based_std_weights = np.mean(moment_based_weights, axis=0), np.std(moment_based_weights, axis=0)
 
     plt.clf()
-    xrange = np.arange(mle_mean_losses.shape[0])
+    xrange = np.arange(mle_mean_weights.shape[0])
     
-    for model_idx in range(mle_mean_losses.shape[1]):
-        color = np.random.rand(3)
-        plt.plot(xrange, mle_mean_losses[:, model_idx], color=color, label=f"Good Model {model_idx}" if model_idx > 0 else f"Bad Model {model_idx}")
-        # plt.fill_between(xrange, mle_mean_losses[:, model_idx] - mle_std_losses[:, model_idx], mle_mean_losses[:, model_idx] + mle_std_losses[:, model_idx], color=color, alpha=0.2)
+    for model_idx in range(mle_mean_weights.shape[1]):
+        color = CB_color_cycle[model_idx]
+        plt.plot(xrange, mle_mean_weights[:, model_idx], color=color, label=f"Good Model" if model_idx > 0 else f"Bad Model")
+        plt.fill_between(xrange, mle_mean_weights[:, model_idx] - mle_std_weights[:, model_idx], mle_mean_weights[:, model_idx] + mle_std_weights[:, model_idx], color=color, alpha=0.2)
 
     plt.xlabel("Number of Iterations")
-    plt.ylabel("Classification loss on data collected")
+    plt.ylabel("Probability using classification loss")
     plt.legend()
     plt.grid(True)
     plt.savefig("mle_widetree_exp.png")
 
     plt.clf()    
-    for model_idx in range(moment_based_mean_losses.shape[1]):
-        color = np.random.rand(3)
-        plt.plot(xrange, moment_based_mean_losses[:, model_idx], color=color, label=f"Good Model {model_idx}" if model_idx > 0 else f"Bad Model {model_idx}")
-        # plt.fill_between(xrange, moment_based_mean_losses[:, model_idx] - moment_based_std_losses[:, model_idx], moment_based_mean_losses[:, model_idx] + moment_based_std_losses[:, model_idx], color=color, alpha=0.2)
+    for model_idx in range(moment_based_mean_weights.shape[1]):
+        color = CB_color_cycle[model_idx]
+        plt.plot(xrange, moment_based_mean_weights[:, model_idx], color=color, label=f"Good Model" if model_idx > 0 else f"Bad Model")
+        plt.fill_between(xrange, moment_based_mean_weights[:, model_idx] - moment_based_std_weights[:, model_idx], moment_based_mean_weights[:, model_idx] + moment_based_std_weights[:, model_idx], color=color, alpha=0.2)
 
     plt.xlabel("Number of Iterations")
-    plt.ylabel("Value Moment loss on data collected")
+    plt.ylabel("Probability using moment based loss")
     plt.legend()
     plt.grid(True)
     plt.savefig("moment_based_widetree_exp.png")
--- src/widetree_sys_id.py
+++ src/widetree_sys_id.py
@@ -39,6 +39,11 @@ def compute_classification_loss(dataset: deque, model: WideTree):
 def run(real_world: WideTree, n_leaves: int, num_models: int, moment_based: bool):
     # Generate model class
     model_class = generate_model_class(n_leaves, num_models)
+    # Hedge weights
+    weights = (1.0/num_models) * np.ones(num_models)
+    all_weights = [weights.copy()]
+    # Hedge epsilon
+    eps = 0.5
     # Choose an initial model
     idx = np.random.randint(num_models)
     model = model_class[idx]
@@ -66,34 +71,47 @@ def run(real_world: WideTree, n_leaves: int, num_models: int, moment_based: bool
         # Update model
         for j in range(num_models):
             losses[j] += compute_model_advantage_loss(dataset, values, model_class[j]) if moment_based else compute_classification_loss(dataset, model_class[j])
+
+        # Update hedge weights
+        for j in range(num_models):
+            loss = compute_model_advantage_loss(dataset, values, model_class[j]) if moment_based else compute_classification_loss(dataset, model_class[j])
+            weights[j] = weights[j] * np.exp(-eps * loss)
+        all_weights.append(weights / np.sum(weights))
         
         # Find best model
         # Do FTL
-        model = model_class[np.argmin(losses)]
-        print(losses)
+        # model = model_class[np.argmin(losses)]
+        # print(losses)
         all_losses.append(copy.deepcopy(losses))
-        print("Selected", np.argmin(losses))
+        # print("Selected", np.argmin(losses))
+
+        # Do Hedge
+        probs = weights / np.sum(weights)
+        model_idx = np.random.choice(np.arange(num_models), p=probs)
+        model = model_class[model_idx]
+        print(weights / np.sum(weights))
+        print("Selected", model_idx)
 
         # Compute policy and values
         policy, values = model.optimal_policy_and_values()
 
-    return np.array(all_losses)
+    return np.array(all_losses), np.array(all_weights)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--n_leaves", type=int, default=100)
-    parser.add_argument("--n_models", type=int, default=5)
+    parser.add_argument("--n_models", type=int, default=2)
     parser.add_argument("--seed", type=int, default=0)
     args = parser.parse_args()
 
     real_world = WideTree(args.n_leaves, "real")
     # MLE
     np.random.seed(args.seed)
-    mle_losses = run(real_world, args.n_leaves, args.n_models, moment_based=False)
+    mle_losses, mle_weights = run(real_world, args.n_leaves, args.n_models, moment_based=False)
     # MOMENT BASED
     np.random.seed(args.seed)
-    moment_based_losses = run(real_world, args.n_leaves, args.n_models, moment_based=True)
+    moment_based_losses, moment_based_weights = run(real_world, args.n_leaves, args.n_models, moment_based=True)
 
-    np.save(f"data/mle_{args.seed}.npy", mle_losses)
-    np.save(f"data/moment_based_{args.seed}.npy", moment_based_losses)
+    np.save(f"data/mle_{args.seed}.npy", mle_weights)
+    np.save(f"data/moment_based_{args.seed}.npy", moment_based_weights)
